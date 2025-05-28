7156df37d6924a812f5268e8bd411c8917e1372d
Amir H. Sadati <eng.amirh.sadati@gmail.com>
2025-05-28 22:24:46 +0000

    Prepare development environment

--- config/settings/base.py
+++ config/settings/base.py
@@ -16,9 +16,10 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
+env_file = os.environ.get("DJANGO_ENV_FILE", BASE_DIR / ".env")
 
 env = environ.Env()
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+environ.Env.read_env(env_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
